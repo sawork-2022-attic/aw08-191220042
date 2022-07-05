2501757af71d3aa4d3d74df84e0ec383a62fe5df
191220042 <1214069454@qq.com>
2022-07-05 14:44:11 +0800

    change file

---
+++ pos-products/src/main/java/com/micropos/product/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.micropos.product.service;
+
+import com.micropos.product.model.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService {
+
+    public List<Product> products();
+
+    public Optional<Product> getProduct(int id);
+
+    public Product randomProduct();
+}
